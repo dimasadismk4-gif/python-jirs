bc3f8b629287f20ef3a92023b049c6aa41ea6ef3
dimasadismk4-gif <dimasadismk4@gmail.com>
2026-09-23 11:42:23 +0700

    tipe data

--- main.py
+++ main.py
@@ -19,3 +19,36 @@ nilai_b = x
 
 #pemanggilan kedua
 print("nilai b = ", nilai_b)
+
+#tipe data integer yang gaada koma 
+data_integer = 1
+
+print("data : ", data_integer)
+print("- bertipe: ", type(data_integer))
+
+#data float 
+data_float = 1.4
+print("data : ", data_float)
+print("- bertipe: ", type(data_float))
+
+#data string
+data_string = "ucup"
+print("data: ", data_string)
+print("- bertipe: ", type(data_string))
+
+#data bool
+data_bool = True
+print("data : ", data_bool)
+print("- bertipe :", type(data_bool))
+
+#data complex
+data_complex = complex(5,6)
+print("data : ", data_complex)
+print("- bertipe :", type(data_complex))
+
+#tipe data dari bahasa c
+from ctypes import c_double 
+
+data_c_double = c_double(10.5)
+print("data : ", data_c_double)
+print("- bertipe :", type(data_c_double))
