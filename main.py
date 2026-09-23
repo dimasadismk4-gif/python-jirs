3c6f6948fc728ca202957659ffe98d4a68ad4f6a
dimasadismk4-gif <dimasadismk4@gmail.com>
2026-09-23 09:00:05 +0700

    first thing to do

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+start_time = time.time()
+
+print("Hello World")
+# cara mengcompile, buka terminal, ketik python3 -m py_compile main.py
+
+print(time.time() - start_time, "detik")
+
+#variabel adalah tempat menyimpan data
+x = 10
+a = 10
+print("nilai x = ", x)
---
+++ test.py
@@ -0,0 +1,5 @@
+print("apakabar my friend")
+#   print("hello world")
+a = 10
+print(a)
+"""ada apa dengan kamu?"""
